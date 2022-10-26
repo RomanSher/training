0dced9637cddf85dd847f386d223a6ef85b9f397
RomanSher <116725160+RomanSher@users.noreply.github.com>
2022-10-26 15:09:38 +0300

    Update config.py

--- python_basic_diploma/config.py
+++ python_basic_diploma/config.py
@@ -3,13 +3,13 @@
 (Токен, API-ключ, website, параметры и url-адреса)
 """
 
-TOKEN = '5454690484:AAFGxjBbMSDDlzgTOdTLxEFL_bb3hfWpKaw'
-KEY = '5e57692579msha43ed0fb195980dp10de22jsnaf2e6df3c387'
+TOKEN = ''
+KEY = ''
 WEBSITE = 'hotels4.p.rapidapi.com'
 url = 'https://hotels4.p.rapidapi.com/locations/search'
 url_list = 'https://hotels4.p.rapidapi.com/properties/list'
 url_photos = 'https://hotels4.p.rapidapi.com/properties/get-hotel-photos'
-db = 'C:/Users/Ксения/Roman/PycharmProject/python_basic_diploma/database/Users.db'
+db = 'python_basic_diploma/database/Users.db'
 client = {
     'X-RapidAPI-Key': KEY,
     'X-RapidAPI-Host': WEBSITE
