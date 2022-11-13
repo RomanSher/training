0903ffdfb8e056d4087594ca91c67e89883a94c4
Roman <116725160+RomanSher@users.noreply.github.com>
2022-11-14 02:04:18 +0300

    Add files via upload

--- test_tasks_2/reviews/reviews/urls.py
+++ test_tasks_2/reviews/reviews/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from app_reviews.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+# Закомментированные строки решение с помощью библиотеки openpyxl
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,11 +30,15 @@ urlpatterns = [
     path('api/manufacturer/<int:pk>/', ManufacturerAPIUpdateDelete.as_view()),
     path('api/car/<int:pk>/', CarAPIUpdateDelete.as_view()),
     path('api/comment/<int:pk>/', CommentAPIUpdateDelete.as_view()),
+    # path('api/export/country/', ExportCountry.as_view({'get': 'dispatch'})),
+    # path('api/export/manufacturer/', ExportManufacturer.as_view({'get': 'dispatch'})),
+    # path('api/export/car/', ExportCar.as_view({'get': 'dispatch'})),
+    # path('api/export/comment/', ExportComment.as_view({'get': 'dispatch'})),
     path('api/export/country/', export_country),
     path('api/export/manufacturer/', export_manufacturer),
     path('api/export/car/', export_car),
     path('api/export/comment/', export_comment),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
