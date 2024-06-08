cc3ff53be91091cc007d49159983c645c36e27d9
Roman <116725160+RomanSher@users.noreply.github.com>
2024-06-08 17:53:37 +0300

    delete test_tasks directory

--- test_tasks/news/app_news/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description']
-
-
-admin.site.register(News, NewsAdmin)
